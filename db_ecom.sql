ebf5706a1393544819011af8eccfee52ed9fbdc7
DemonicCoder <semaltysourabh@gmail.com>
2020-08-24 12:59:52 +0530

    update db

---
+++ db_ecom.sql
@@ -0,0 +1,133 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 24, 2020 at 09:29 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_ecom`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_categories`
+--
+
+CREATE TABLE `tbl_categories` (
+  `id` int(11) NOT NULL,
+  `category_name` varchar(100) NOT NULL,
+  `category_img` text NOT NULL,
+  `slug` varchar(100) NOT NULL,
+  `meta_title` varchar(100) NOT NULL,
+  `meta_desc` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_categories`
+--
+
+INSERT INTO `tbl_categories` (`id`, `category_name`, `category_img`, `slug`, `meta_title`, `meta_desc`) VALUES
+(67, 'Men', 'products/category/2062376157MensCategory.jpg', 'Mens Clothing', 'Clothing for Mens', 'Huge round of products for mens'),
+(68, 'Womens', 'products/category/2035099435WomenCategory.jpg', 'Womens Clothing', 'Clothing Style for Womens', 'huge round of products for Womens');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_subcategories`
+--
+
+CREATE TABLE `tbl_subcategories` (
+  `id` int(11) NOT NULL,
+  `sub_category_name` varchar(100) NOT NULL,
+  `sub_category_img_name` text NOT NULL,
+  `categoryId` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_subcategories`
+--
+
+INSERT INTO `tbl_subcategories` (`id`, `sub_category_name`, `sub_category_img_name`, `categoryId`) VALUES
+(30, 'Footwear', 'products/subCategory/2294047287sc_footwear.jpg', '67');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userdetails`
+--
+
+CREATE TABLE `userdetails` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `userdetails`
+--
+
+INSERT INTO `userdetails` (`id`, `email`, `password`) VALUES
+(1, 'semaltysourabh@gmail.com', '123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_categories`
+--
+ALTER TABLE `tbl_categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_subcategories`
+--
+ALTER TABLE `tbl_subcategories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `userdetails`
+--
+ALTER TABLE `userdetails`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_categories`
+--
+ALTER TABLE `tbl_categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
+
+--
+-- AUTO_INCREMENT for table `tbl_subcategories`
+--
+ALTER TABLE `tbl_subcategories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `userdetails`
+--
+ALTER TABLE `userdetails`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
